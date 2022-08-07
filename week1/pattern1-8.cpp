fd9745b67777d1b2e441f0426b52e78d45e4efe2
satyaphani2404 <satyaphani2404@gmail.com>
2022-08-07 15:09:22 +0530

    first commit

--- week1/pattern1-8.cpp
+++ week1/pattern1-8.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include<cmath>
 using namespace std;
+int main(){
+    int n;
+    int c=0;
+    cin>>n;
+    for(int i=1;i<2*n;i++){
+        (i>n)?c--:c++;
+        for(int j=1;j<=c;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+
+    }
+}
 // logic one
 // int main(){
 //     int n;
@@ -20,15 +33,18 @@ using namespace std;
 //     return 0;
 // }
 // 2nd logic
-int main()
- {
-    int n;
-    cin>>n;
-    for(int i=n;i>=-(n-1);i--){
-        for(int j=(n-1);j>=abs(i);j--){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-}
+// int main()
+//  {
+//     int n;
+//     cin>>n;
+//     for(int i=n;i>=-(n-1);i--){
+//         for(int j=(n-1);j!=abs(i);j--){
+//             cout<<" ";
+//         }
 
+//         for(int j=(n-1);j>=abs(i);j--){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
