fabb40c035c5845882c25306c8e8ced93959e34e
satyaphani2404 <satyaphani2404@gmail.com>
2022-08-07 17:42:33 +0530

    first commit

---
+++ week1/pattern1-14.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int in=1,fa=2*n-1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<2*n;j++){
+            if(i==j){
+                cout<<"*";
+            }
+            else if(i==in && j== fa){
+                cout<<"*";
+                in++;
+                fa--;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
