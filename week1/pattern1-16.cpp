e4686e5514db4a1ab5d02e7e1f4bf9cfb0262e62
satyaphani2404 <satyaphani2404@gmail.com>
2022-08-08 12:51:02 +0530

    first commit

---
+++ week1/pattern1-16.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<2*n;j++){
+            if(i+j>=2*n||i==j||i-j<n && i-j >0){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
