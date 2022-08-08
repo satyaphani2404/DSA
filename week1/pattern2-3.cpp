3f199f3fe2f8435bdede2e40d8c83f3bbd953ef8
satyaphani2404 <satyaphani2404@gmail.com>
2022-08-08 19:46:02 +0530

    first commit

---
+++ week1/pattern2-3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=n;i>0;i--){
+        for(int j=0;j<=n;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
