6396103b1e6eb5c6cc0b0aa312c769c0b49763a6
satyaphani2404 <satyaphani2404@gmail.com>
2022-08-10 12:58:05 +0530

    first commit

---
+++ week1/pattern3-16.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,a=64;
+    cin>>n;
+    int c=n;
+    for(int i=1;i<=2*n;i++){
+        
+
+        for(int j=n;j>=c;j--){
+            cout<<char(a+j)<<" ";
+        }
+        (i>=n)?c++:c--;
+        cout<<endl;
+
+    }
+    
+}
+// F
+// FE
+// FED
+// FEDC
+// FEDCB
+// FEDCBA
+// FEDCB
+// FEDC
+// FED
+// FE
+// F
